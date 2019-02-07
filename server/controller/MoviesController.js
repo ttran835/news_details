e3c5fad45b911f7cea49b737203d44b1983dac6e
tim <timothy.tran835@gmail.com>
2019-02-06 18:25:42 -0800

    able to successfully get movie from api. Next step is to save movies into api, then front-end skeleton

--- server/controller/MoviesController.js
+++ server/controller/MoviesController.js
@@ -1,9 +1,23 @@
 require('dotenv').config();
+const Axios = require('axios');
 const express = require('express');
+
+const url = `https://api.simkl.com/search/movie?q&client_id=${
+  process.env.SIMKL_API
+}`;
 
 const MoviesController = {
   get: (req, res) => {
-    res.status(200).send('hello from homeGet');
+    Axios.get(url)
+      .then(response => {
+        if (response) {
+          console.log(response.data);
+          res.status(200).send(response.data);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
   },
 
   post: (req, res) => {
