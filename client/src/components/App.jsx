30be27e6f0f9022452359cc81da176e1d65df762
Tim Tran <timothy.tran835@gmail.com>
2019-03-31 23:44:59 -0700

    moved components to specific folders and made adjustments to css to be more readable

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 
 // components
-import Link from '../components/Link/Link';
-import AllNews from '../components/AllNews/AllNews';
+import AllNews from '../components/NewsComponents/AllNews/AllNews';
 import { request } from 'https';
 
 export default class App extends Component {
@@ -16,21 +15,16 @@ export default class App extends Component {
 
     this.getNews = this.getNews.bind(this);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-    Axios.defaults.baseURL = `localhost:${process.env.SERVER_PORT}`;
+    Axios.defaults.baseURL = `http://localhost:${process.env.SERVER_PORT}`;
     // Axios.defaults.baseURL =
   }
 
   componentDidMount() {
-    // this.getNews();
-    console.log(Axios.defaults.baseURL);
+    this.getNews();
   }
 
   getNews() {
-    Axios.get('/all').then(res => {
-      console.log(res.data);
+    Axios.get('/home').then(res => {
       this.setState({
         news: res.data,
       });
@@ -39,11 +33,6 @@ export default class App extends Component {
 
   handleChange(event) {
     this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    console.log(`information submitted, ${this.state.value}`);
-    // event.preventDefault();
   }
 
   render() {
@@ -69,3 +58,20 @@ export default class App extends Component {
     );
   }
 }
+
+/*
+Example news data; 
+{
+    "_id": 1,
+    "source": [["id", "the-new-york-times"], ["name", "The New York Times"]],
+    "author": "DAVE PHILIPPS",
+    "title": "White Supremacism in the U.S. Military, Explained",
+    "description": "For much of the 20th century, the Ku Klux Klan actively recruited members in the armed forces without hindrance. Dozens of Navy sailors in uniform attended a Klan rally in the summer of 1923, where they were photographed holding their robes.",
+    "url": "https://www.nytimes.com/2019/02/27/us/military-white-nationalists-extremists.html",
+    "urlToImage": "https://static01.nyt.com/images/2019/02/28/us/28military-1-print/23Military-2-facebookJumbo.jpg",
+    "publishedAt": "2019-02-27T10:00:08Z",
+    "content": "Even so, the military, reflecting society as a whole, still struggled with hate groups and racist violence.\r\nExperts generally agree that the problem is more widespread than the military acknowledges.\r\nIn 1995, after the Oklahoma City bombing and the killing … [+1507 chars]",
+    "createdAt": "2019-03-04T02:29:14.966Z",
+    "updatedAt": "2019-03-04T02:29:14.966Z"
+  },
+*/
---
+++ client/src/components/NewsComponents/AllNews/AllNews.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './AllNews.module.scss';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+export default function AllNews(props) {
+  return (
+    <div className="container">
+      <div className={`row justify-content-center`}>
+        <h1
+          className={`col-xs-12${styles.testThings} ${styles.materialUiShadow}`}
+        >
+          Why cant you stop stealing my msuci!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+// AllNews.PropTypes = {
+//   news: PropTypes.string,
+// };
+
+/*
+Example news data; 
+{
+    "_id": 1,
+    "source": [["id", "the-new-york-times"], ["name", "The New York Times"]],
+    "author": "DAVE PHILIPPS",
+    "title": "White Supremacism in the U.S. Military, Explained",
+    "description": "For much of the 20th century, the Ku Klux Klan actively recruited members in the armed forces without hindrance. Dozens of Navy sailors in uniform attended a Klan rally in the summer of 1923, where they were photographed holding their robes.",
+    "url": "https://www.nytimes.com/2019/02/27/us/military-white-nationalists-extremists.html",
+    "urlToImage": "https://static01.nyt.com/images/2019/02/28/us/28military-1-print/23Military-2-facebookJumbo.jpg",
+    "publishedAt": "2019-02-27T10:00:08Z",
+    "content": "Even so, the military, reflecting society as a whole, still struggled with hate groups and racist violence.\r\nExperts generally agree that the problem is more widespread than the military acknowledges.\r\nIn 1995, after the Oklahoma City bombing and the killing … [+1507 chars]",
+    "createdAt": "2019-03-04T02:29:14.966Z",
+    "updatedAt": "2019-03-04T02:29:14.966Z"
+  },
+*/
